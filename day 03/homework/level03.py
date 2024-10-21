9870e90d385695a59ecad747fbe2fdc58abcca89
niko777-lab <nikoloziobidze487@gmail.com>
2024-10-21 20:35:41 +0400

    “commit”

---
+++ day 03/homework/level03.py
@@ -0,0 +1,12 @@
+name = "nikoloz"
+surname = "iobidze"
+Country = "georgia"
+Hobbie = "programing" #not a hacker just new to it and so far like it so much :) 
+age = "11"
+
+print(name + " " + surname + " " + age + " " + Country + " " + Hobbie)
+
+
+
+
+
