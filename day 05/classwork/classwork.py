28289e717acbce0b52723556f9aa2616133ebc9f
niko777-lab <nikoloziobidze487@gmail.com>
2024-11-03 20:28:32 +0400

    commit

---
+++ day 05/classwork/classwork.py
@@ -0,0 +1,51 @@
+# 1
+greeting = "Hello"
+userName = "student"
+age = 20
+location = "New York"
+
+#2
+welcome = "welcome"
+userName = "niko"
+! = !
+print('Welcome' + "username" + "!") 
+print("greeting" + " " + "username" + "!") 
+
+
+#3
+age = 11
+in 5 years = "in 5 years"
+
+print("age")  
+print("In 5 years" + "age" + 5) 
+
+#4
+favoriteNumber = 7
+print(favoriteNumber * 2)  
+
+#5
+total_score = 100
+bonus_points = 10
+finalScore = "total_score" + "bonus_points"
+print(finalScore)
+
+#6
+days_in_a_week = "7"
+print("There are " + days_in_a_week * "4" + " days in four weeks")
+
+#7
+firstName = "John"
+print("Hello" + " " + "lastName" + "!")
+
+#8
+nickname = "Ace
+print("nickname")
+
+#9
+year = 2024
+print("Next year is " + year + 1)
+print("Your age next year will be " + age + 1)
+
+#10
+farewell = "Goodbye"
+print("FareWell"+ "!")
