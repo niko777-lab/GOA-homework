395a34a015c5fd00875b70ef990aac6cbd0a4d34
niko777-lab <nikoloziobidze487@gmail.com>
2024-12-17 18:48:50 +0400

    commit

---
+++ group 54/day 20/classwork/day 20.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print("nikoloz iiobidze")
+
+
+# for i in range(11,40,4):
+#     print(i)
+
+# for i in range(11, 40,2):
+#     print(i)
+
+num_sum=0
+for i in range(2,31,2):
+    num_sum=num_sum+i
+print(num_sum)
+ 
+
---
+++ group 54/day 20/homework/day 20.py
@@ -0,0 +1,24 @@
+for x in range(10,30):
+    print(x)
+
+for x in range(20,41):
+    print(x)
+
+for x in range(50,61,2):
+    print(x)
+
+for x in range(11,50,2):
+    print(x)
+
+number_sum=0
+for x in range(10,30):
+    number_sum=number_sum+x
+print(number_sum)
+
+number_sum=0
+for x in range(2,30,2):
+    number_sum=number_sum+x
+print(number_sum)
+
+for x in range(2,30,2):
+    print(x)
