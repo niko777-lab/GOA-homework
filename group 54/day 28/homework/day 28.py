32acdae39eab7be28bdfb4745f811ba35e900c69
niko777-lab <nikoloziobidze487@gmail.com>
2025-01-13 13:23:15 +0400

    commit

---
+++ group 54/day 28/homework/day 28.py
@@ -0,0 +1,51 @@
+# listარის სიასის გაკეთება ჩვენ შეგვიძლია შევინახოთ ცვლადში index არის მისი მისამართი მაგალითად ასევე index-ი იწყება 0-ლით
+#indexing არის მაგალითად ეს დავწერეთ მარამ ახლა ამ list-იდან მინდა გამოვიტანო ერთი მთლიანი არა ამისთვის ჩვენ ვაკეთებთ ასე   ჯერ print  მერე list-ის სახელი შემდეგ []- ეს და [] ამასი დავწერთ მაგის index-ს ასე print(shopping_list[2]) mutable არის მაგალითად უკვე დავწერეთ რაგაც მააგრამ მისი შეცვლა გვინდა ამას ქვია mutable კეთდება ასე  shopping_list[4] += beens  და immutable არის სადაც ვერ შევცვლით ამას
+# მაგ.
+#    მენულე index     პირველი index    მეორე index
+#                   0       1        2
+shopping_ist = ["tomato","apple","orange"]
+#  3) Create a list of five numbers and print the first, third, and last elements using positive indexing. positive indexing - დადებითი ინდექსები
+
+num_list=10,22,335,12,224
+print(num_list[0])
+print(num_list[2])
+print(num_list[4])
+
+# 4) Create a list of 20 elements (any data type) and print all of the elements - use indexing
+num_list2 = 1132,446,579,421,445,224,224,2432,4325,66,89,7,5,45,3,34,54,6,68,54
+print(num_list2[0])
+print(num_list2[1])
+print(num_list2[2])
+print(num_list2[3])
+print(num_list2[4])
+print(num_list2[5])
+print(num_list2[6])
+print(num_list2[7])
+print(num_list2[8])
+print(num_list2[9])
+print(num_list2[10])
+print(num_list2[11])
+print(num_list2[12])
+print(num_list2[13])
+print(num_list2[14])
+print(num_list2[15])
+print(num_list2[16])
+print(num_list2[17])
+print(num_list2[18])
+print(num_list2[19])
+
+
+# 5) Create a list of five numbers and print the first, third, and last elements using negative indexing. negative indexing - უარყოფითი ინდექსები
+
+num_nagative=12,353,7,82,2
+print(num_nagative[-5])
+print(num_nagative[-4])
+print(num_nagative[-3])
+print(num_nagative[-2])
+print(num_nagative[-1])
+
+# 6) ask user for name and save it in variable. use indexing and print first and last symbols of name string
+
+user_name = str(input("Enter ur name :"))
+print(user_name[0])
+print(user_name[3])
