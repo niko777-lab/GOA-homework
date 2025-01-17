3142ce97e9b58bae79bf206c9d1b0a32e514f5db
niko777-lab <nikoloziobidze487@gmail.com>
2025-01-17 22:11:27 +0400

    commit

---
+++ group 54/day 28/homework/Extra_work.py
@@ -0,0 +1,11 @@
+# def create_name(first,last):
+#     first = first()
+#     last = last()
+#     return first + " " + last
+
+# full_name = create_name("niko","iobidze")
+
+# print(full_name)
+double = lambda x: x * 2
+
+print(double[2])
