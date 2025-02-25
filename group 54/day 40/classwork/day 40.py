3caa6ced14b18f4d4abbc4b83d7a90907e455b1b
niko777-lab <nikoloziobidze487@gmail.com>
2025-02-25 18:30:37 +0400

    commit

---
+++ group 54/day 40/classwork/day 40.py
@@ -0,0 +1,8 @@
+def greet(name, surname, age, academy, favourite_color, city):
+    return f"Hello, my name is {name}, my surname is {surname}. I am {age} years old. I study in {academy}. My favourite color is {favourite_color}. I live in {city}."
+
+# ფუნქციის გამოძახება
+greeting = greet("John", "Doe", 25, "GOA", "blue", "New York")
+
+# დაბეჭდვა
+print(greeting)
